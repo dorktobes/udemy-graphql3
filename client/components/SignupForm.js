796ef69072f52cb12374674ac5c917051924e261
Toby Hoshman <t.hoshman@gmail.com>
2018-04-11 22:28:41 -0700

    implemented SignUp component and mutations

---
+++ client/components/SignupForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import AuthForm from './AuthForm';
+import mutation from '../mutations/Signup';
+import query from '../queries/CurrentUser';
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: [],
+    }
+  }
+
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: {
+        email,
+        password,
+      },
+      refetchQueries: [{ query }],
+    }).catch((err) => {
+      const errors = err.graphQLErrors.map(e => e.message);
+      this.setState({ errors });
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h3>Sign Up</h3>
+        <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(Signup);
